01aa7e8ab1d1992c210741aaa186511751c91d3f
Inderjit57 <singhinderjit57@yahoo.com>
2022-01-10 22:45:54 -0500

    completed

--- src/Question3.java
+++ src/Question3.java
@@ -1,34 +1,30 @@
+
 import java.util.Scanner;
 
 public class Question3 {
 
 	public static void main(String[] args) {
 		/*
-		 * Q3) Write a program that prompts the user to input a positive integer. 
-		 * It should then output a message indicating whether the number is a prime number or not.
+		 * Q3) Write a program that prompts the user to input a positive integer. It
+		 * should then output a message indicating whether the number is a prime number
+		 * or not.
 		 * 
-		 * Conditons or a prime number
-		 * x>1
-		 * x%x == 0
-		 * x%1 == 0 
-		 * prime number should only have two factors 
-		 * X can’t be divided Y =0 
-		 * If x divided by y the prime number should be greater then 0
-		 * 
-		 */		
-		
+		 * Conditons for a prime number x>1 x%x == 0 x%1 == 0
+		 * Every prime number is an odd number except number 2.
+		 */
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number");
-		
-		int number1 = sc.nextInt();
-		
-		while(number1 <=20) {
-			if (number1%1 == 0 && number1%number1 == 0 && number1>1) {
-				System.out.println("Prime number");
-				number1 += 1;
-			}
+
+		int number = sc.nextInt();
+
+		// number which are not divisible by 2 and 3 
+		//
+		if (number % 2 != 0 && number % 3 != 0 && number > 1) {
+			System.out.println(number + "  = Prime number");
+		} else {
+			System.out.println(number + "  = not Prime number");
 		}
-		
 
 	}
 
