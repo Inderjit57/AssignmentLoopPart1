1d6d4a2f705c611cc7a452006ea55caca51d2161
Inderjit57 <singhinderjit57@yahoo.com>
2022-01-12 17:06:57 -0500

    Created codes using DoWhile and While condition

--- src/Question2_UsingDoWhile.java
+++ src/Question2_UsingDoWhile.java
@@ -1,16 +1,9 @@
 import java.util.Scanner;
 
-public class Question2 {
+public class Question2_UsingDoWhile {
 
 	public static void main(String[] args) {
-		/*
-		 * Q2) There is a water tank of capacity 100 litres. Water is filled into it
-		 * using bucket which have capacity of 10 litres. Write a program which fills
-		 * the tank to its capacity using these buckets. Also include a mechanism in
-		 * your program to stop the tank from over flowing.
-		 */
-
-		Scanner scan = new Scanner(System.in);
+Scanner scan = new Scanner(System.in);
 		
 		// input in the console will be from 10 to 100; 10,20,30....100.
 		// 1 bucket = 10liters
@@ -18,7 +11,7 @@ public class Question2 {
 
 		double bucketVolume = scan.nextDouble();	//Bucket capacity is 10liters 
 
-		while (bucketVolume <= 100) {
+		do {
 			System.out.println("tank is at: " + bucketVolume + " liters");
 			System.out.println("Tank capacity left: " + (100 - bucketVolume) + " liters"); // Space left in the tank
 
@@ -28,7 +21,7 @@ public class Question2 {
 				System.out.println("Tank is empty");
 			}
 			break;
-		}
+		} while (bucketVolume <= 100);
 
 	}
 
